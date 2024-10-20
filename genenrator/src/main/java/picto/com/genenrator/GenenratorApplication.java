52d146216c11cda469bd1b9f46c3544eee78866e
ak4700159 <ak47001@naver.com>
2024-10-20 20:51:35 +0900

    generator creates

---
+++ genenrator/src/main/java/picto/com/genenrator/GenenratorApplication.java
@@ -0,0 +1,13 @@
+package picto.com.genenrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenenratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenenratorApplication.class, args);
+    }
+
+}
---
+++ genenrator/src/test/java/picto/com/genenrator/GenenratorApplicationTests.java
@@ -0,0 +1,13 @@
+package picto.com.genenrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenenratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
