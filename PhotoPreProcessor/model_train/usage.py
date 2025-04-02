44b037dc116538aae66a2e17458972b1b85502f4
ak4700159 <ak47001@naver.com>
2025-04-02 14:23:12 +0900

    JsonExtractor added

--- PhotoPreProcessor/model_train/usage.py
+++ PhotoPreProcessor/model_train/usage.py
@@ -1,5 +1,7 @@
 import torch
 from real_fake_model import SimpleCNN
+from efficientnet_model import EfficientNetClassifier
+
 
 def detect():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -8,9 +10,11 @@ def detect():
     # 불필요한 train_loader 등은 None으로 대체 가능
     dummy_loader = []
 
-    model = SimpleCNN(dummy_loader, dummy_loader, device)
-    model.load_model("model_last_epoch.pth")
+    model = EfficientNetClassifier(dummy_loader, dummy_loader, device)
+    model.load_model("efficientnet_real_fake.pth")
 
-    image_path = "C:/capstone_workspace/PICTO-BackEnd/PhotoPreProcessor/images/sample.jpg"  # 예시 경로
+    image_path = "C:/capstone_workspace/PICTO-BackEnd/PhotoPreProcessor/model_train/sample/합성사진4.jpg"  # 예시 경로
     result = model.predict_image(image_path)
     print(f"🖼️ '{image_path}' 예측 결과: {result}")
+
+detect()
