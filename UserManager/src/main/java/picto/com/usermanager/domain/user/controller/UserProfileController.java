f91c0cbe59248aff3c33fd45682540a555eb7e69
ak4700159 <ak47001@naver.com>
2025-05-06 19:58:36 +0900

    05/06 19:58 error msg added

--- UserManager/src/main/java/picto/com/usermanager/domain/user/controller/UserProfileController.java
+++ UserManager/src/main/java/picto/com/usermanager/domain/user/controller/UserProfileController.java
@@ -12,12 +12,14 @@ public class UserProfileController {
     private UserProfileService userProfileService;
 
     @PutMapping("/user-manager/profile/photo")
-    public ResponseEntity<String> updateUserProfile(@RequestParam("userId") Long userId, @RequestParam("photoId") Long photoId ) {
+    public ResponseEntity<String> updateUserProfile(@RequestParam("userId") Long userId, @RequestParam("photoId") Long photoId) {
         try {
             userProfileService.updateProfilePhoto(userId, photoId);
-            return  ResponseEntity.ok("Profile photo updated successfully");
+            return ResponseEntity.ok("Profile photo updated successfully");
         } catch (Exception e) {
-            return ResponseEntity.status(404).build();
+            return ResponseEntity
+                    .status(404)
+                    .body("프로필 사진 업데이트 실패: " + e.getMessage());
         }
     }
 
@@ -25,9 +27,11 @@ public class UserProfileController {
     public ResponseEntity<String> deleteUserProfile(@RequestParam("userId") Long userId) {
         try {
             userProfileService.removeProfilePhoto(userId);
-            return  ResponseEntity.ok("Profile photo removed successfully");
+            return ResponseEntity.ok("Profile photo removed successfully");
         } catch (Exception e) {
-            return ResponseEntity.status(404).build();
+            return ResponseEntity
+                    .status(404)
+                    .body("프로필 사진 삭제 실패: " + e.getMessage());
         }
     }
 }
