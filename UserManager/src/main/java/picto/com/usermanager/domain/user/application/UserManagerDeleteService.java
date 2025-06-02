844363041f8a30287c14ee7a0e76f43534e46e32
AhnSH01 <tlgud9903@naver.com>
2025-06-02 15:37:48 +0900

    06/02 15:37 Patch 및 Delete 시 Keycloak에서도 수행

--- UserManager/src/main/java/picto/com/usermanager/domain/user/application/UserManagerDeleteService.java
+++ UserManager/src/main/java/picto/com/usermanager/domain/user/application/UserManagerDeleteService.java
@@ -9,6 +9,11 @@ import picto.com.usermanager.domain.user.dao.UserRepository;
 import picto.com.usermanager.domain.user.dto.request.EventRequest;
 import picto.com.usermanager.domain.user.dto.request.UserRequest;
 import picto.com.usermanager.domain.user.entity.*;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.UserRepresentation;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 @RequiredArgsConstructor
@@ -17,33 +22,72 @@ public class UserManagerDeleteService {
     private final MarkRepository markRepository;
     private final BlockRepository blockRepository;
 
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String baseUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.mail.username}")
+    private String email;
+
+    private String getKeycloakServerUrl() {
+        return baseUrl;
+    }
+
     @Transactional
-    public void deleteUSer(UserRequest request){
+    public void deleteUSer(UserRequest request) {
         try {
+            // Keycloak에서 사용자 삭제
+            Keycloak keycloak = KeycloakBuilder.builder()
+                    .serverUrl(getKeycloakServerUrl())
+                    .realm(realm)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .grantType("client_credentials")
+                    .build();
+
+            List<UserRepresentation> users = keycloak.realm(realm).users().search(request.getAccountName(), null, null,
+                    request.getEmail(),
+                    0, 1);
+            if (!users.isEmpty()) {
+                UserRepresentation user = users.get(0);
+                keycloak.realm(realm).users().delete(user.getId());
+            }
+
+            // DB에서 사용자 삭제
             userRepository.delete(userRepository.getReferenceById(request.getUserId()));
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("회원 탈퇴 실패");
         }
     }
 
     @Transactional
-    public void deleteMark(EventRequest request) throws IllegalAccessException{
-        try{
+    public void deleteMark(EventRequest request) throws IllegalAccessException {
+        try {
             User sourceUser = userRepository.getReferenceById(request.getSourceId());
             User targetUser = userRepository.getReferenceById(request.getTargetId());
-            markRepository.delete(new Mark(new MarkId(sourceUser.getUserId(), targetUser.getUserId()),targetUser, sourceUser));
-        }catch (Exception e){
+            markRepository.delete(
+                    new Mark(new MarkId(sourceUser.getUserId(), targetUser.getUserId()), targetUser, sourceUser));
+        } catch (Exception e) {
             throw new IllegalAccessException(e.getMessage());
         }
     }
 
     @Transactional
-    public void deleteBlock(EventRequest request) throws IllegalAccessException{
-        try{
+    public void deleteBlock(EventRequest request) throws IllegalAccessException {
+        try {
             User sourceUser = userRepository.getReferenceById(request.getSourceId());
             User targetUser = userRepository.getReferenceById(request.getTargetId());
-            blockRepository.delete(new Block(new BlockId(sourceUser.getUserId(), targetUser.getUserId()),sourceUser, targetUser));
-        }catch (Exception e){
+            blockRepository.delete(
+                    new Block(new BlockId(sourceUser.getUserId(), targetUser.getUserId()), sourceUser, targetUser));
+        } catch (Exception e) {
             throw new IllegalAccessException(e.getMessage());
         }
     }
--- UserManager/src/main/java/picto/com/usermanager/domain/user/application/UserManagerPatchService.java
+++ UserManager/src/main/java/picto/com/usermanager/domain/user/application/UserManagerPatchService.java
@@ -2,11 +2,17 @@ package picto.com.usermanager.domain.user.application;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import picto.com.usermanager.domain.user.dao.*;
 import picto.com.usermanager.domain.user.dto.request.*;
 import picto.com.usermanager.domain.user.entity.*;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.UserRepresentation;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -18,28 +24,68 @@ public class UserManagerPatchService {
     private final TagSelectRepositroy tagSelectRepositroy;
     private final FilterRepository filterRepository;
 
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String baseUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
+    private String clientSecret;
+
+    private String getKeycloakServerUrl() {
+        return baseUrl;
+    }
+
     @Transactional
     public void fetchUser(UserRequest request) throws IllegalAccessException {
         User findUser = userRepository.getReferenceById(request.getUserId());
         if (findUser != null) {
+            // Keycloak 연결 설정
+            Keycloak keycloak = KeycloakBuilder.builder()
+                    .serverUrl(getKeycloakServerUrl())
+                    .realm(realm)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .grantType("client_credentials")
+                    .build();
+
+            List<UserRepresentation> users = keycloak.realm(realm).users().search(request.getAccountName(), null, null,
+                    request.getEmail(),
+                    0, 1);
+            UserRepresentation user = users.get(0);
+
             // 비밀번호 변경인 경우
-            if(request.getType().equals("password")){
-                BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-                String newPasswrod = passwordEncoder.encode(request.getPassword());
-                findUser.setPassword(newPasswrod);
+            if (request.getType().equals("password")) {
+                findUser.setPassword(request.getPassword());
+
+                // Keycloak 비밀번호 업데이트
+                CredentialRepresentation credential = new CredentialRepresentation();
+                credential.setType(CredentialRepresentation.PASSWORD);
+                credential.setValue(request.getPassword());
+                credential.setTemporary(false);
+                keycloak.realm(realm).users().get(user.getId()).resetPassword(credential);
             }
             // 사용자 정보 변경
-            else if(request.getType().equals("info")){
+            else if (request.getType().equals("info")) {
                 findUser.setAccountName(request.getAccountName());
                 findUser.setEmail(request.getEmail());
                 findUser.setName(request.getName());
                 findUser.setIntro(request.getIntro());
                 findUser.setProfileActive(request.getProfileActive());
                 findUser.setProfilePhotoPath(request.getProfilePhotoPath());
+
+                // Keycloak 사용자 정보 업데이트
+                user.setUsername(request.getAccountName());
+                user.setEmail(request.getEmail());
+                user.setAttributes(Collections.singletonMap("name", Collections.singletonList(request.getName())));
+                keycloak.realm(realm).users().get(user.getId()).update(user);
             }
             userRepository.save(findUser);
-        }
-        else {
+        } else {
             throw new IllegalAccessException("NOT FOUND USER");
         }
     }
@@ -53,38 +99,36 @@ public class UserManagerPatchService {
             findSetting.setAutoRotation(request.isAutoRotation());
             findSetting.setPopularAlert(request.isPopularAlert());
             userSettingRepositroy.save(findSetting);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             throw new IllegalAccessException(e.getMessage());
         }
     }
 
     @Transactional
     public void fetchMark(EventRequest request) throws IllegalAccessException {
-        try{
+        try {
             User sourceUser = userRepository.getReferenceById(request.getSourceId());
             User targetUser = userRepository.getReferenceById(request.getTargetId());
             Mark mark = new Mark(new MarkId(sourceUser.getUserId(), targetUser.getUserId()), targetUser, sourceUser);
-            if(!markRepository.existsById(mark.getId())){
+            if (!markRepository.existsById(mark.getId())) {
                 markRepository.save(mark);
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalAccessException(e.getMessage());
         }
     }
 
     @Transactional
     public void fetchBlock(EventRequest request) throws IllegalAccessException {
-        try{
+        try {
             User sourceUser = userRepository.getReferenceById(request.getSourceId());
             User targetUser = userRepository.getReferenceById(request.getTargetId());
-            Block block = new Block(new BlockId(sourceUser.getUserId(), targetUser.getUserId()), targetUser, sourceUser);
-            if(!blockRepository.existsById(block.getId())){
+            Block block = new Block(new BlockId(sourceUser.getUserId(), targetUser.getUserId()), targetUser,
+                    sourceUser);
+            if (!blockRepository.existsById(block.getId())) {
                 blockRepository.save(block);
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             throw new IllegalAccessException(e.getMessage());
         }
     }
@@ -95,11 +139,11 @@ public class UserManagerPatchService {
         try {
             User findUser = userRepository.getReferenceById(request.getUserId());
             tagSelectRepositroy.deleteByUserId(request.getUserId());
-            for(String tagName : request.getTagNames()) {
-                TagSelect tag = new TagSelect(new TagSelectId(tagName, findUser.getUserId()),findUser);
+            for (String tagName : request.getTagNames()) {
+                TagSelect tag = new TagSelect(new TagSelectId(tagName, findUser.getUserId()), findUser);
                 tagSelectRepositroy.save(tag);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalAccessException(e.getMessage());
         }
     }
@@ -111,7 +155,7 @@ public class UserManagerPatchService {
             filter.setSort(request.getSort());
             filter.setPeriod(request.getPeriod());
             filterRepository.save(filter);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalAccessException(e.getMessage());
         }
     }
